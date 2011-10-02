1892f34e23e59277a66718c36d9c93eaa928065d
Christopher Brown <io@henrian.com>
2011-10-02 12:58:45 -0500

    Simpler looseForm parsing.

--- static/js/jquery-forms.js
+++ static/js/jquery-forms.js
@@ -5,36 +5,28 @@ if (window.console === undefined) {
 function parseLooseForm($form) {
   var store = {};
   $form.children('div').each(function(i, div) {
-    var field = div.id,
-        $div = $(div),
+    var $div = $(div),
         value = [],
         force_list = false;
+    
     $div.find("input[type='text']").each(function(i, el) {
       value.push(el.value);
     });
     $div.find("input[type='password']").each(function(i, el) {
       value.push(el.value);
     });
-    // for each checkbox get the id, find the label[for=<that-id>].innerText, use that as value
-    $div.find("input[type='checkbox']").each(function(i, el) {
+    if ($div.find("input[type='checkbox']").length > 1) {
       force_list = true;
-      var $el = $(el);
-      if ($el.prop('checked')) {
-        var text = $div.find("label[for='" + el.id + "']").text();
-        value.push(text);
-      }
-    });
-    
-    var $radio = $div.find("input[type='radio']:checked");
-    if ($radio.length > 0) {
-      var text = $div.find("label[for='" + $radio.attr('id') + "']").text();
-      value.push(text);
     }
 
-    // var text_value = $div.find("input[type='text']").map(function(i, el) { return el.value; });
-    // if (text_value !== undefined && text_value !== null) {
-    //   value.push(text_value);
-    // }
+    // for each checkbox/radiobutton get the id, find the label[for=<that-id>].innerText, use that as value
+    $div.find("input[type='checkbox']:checked").each(function(i, el) {
+      value.push($div.find("label[for='" + el.id + "']").text());
+    });
+    $div.find("input[type='radio']:checked").each(function(i, el) {
+      value.push($div.find("label[for='" + el.id + "']").text());
+    });
+
     if (!force_list && value.length < 2) {
       value = value[0];
     }
