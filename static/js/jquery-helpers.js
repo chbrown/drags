dcea4d0fe86c56195f27fa6ad4302752a8ff39ca
Christopher Brown <io@henrian.com>
2012-02-01 17:39:10 -0600

    Rewrote preloader queue.

--- static/js/jquery-helpers.js
+++ static/js/jquery-helpers.js
@@ -66,143 +66,107 @@ function timestamp() { return (new Date()).getTime(); }
     // each url, on loading, will be stuck in the dom, in a hidden div.
     // keyed by their santized urls as element ids, i.e. url.replace(/\W/g, '')
     this.urls = urls;
-    this.cache = {}; // keyed by url. The value is null while being loaded, and the element when completed.
-    this.callbacks = {}; // keyed by url, lists of callbacks for when a particular movie is done.
-    this.progresses = {}; // keyed by url, jQuery elements | undefined
-    this.currently_loading_url = '';
-    // this.index = 0;
-    this.paused = false;
-    this.processing_queue = false;
     this.$buffer = $buffer;
-    this.timeouts = {zero: 20, slow: 80, hard: 200, wait: 200, loop: 250};
     this.debug = false;
     if (debug !== undefined)
       this.debug = debug;
     else if (typeof Cookie !== 'undefined' && Cookie.get('debug') === 'true')
       this.debug = true;
-    // this.processQueue(); // don't automatically start
-  }
-  Preloader.prototype.pauseQueue = function() {
-    this.paused = true;
-    this.processing_queue = false;
-  };
-  Preloader.prototype.abortPreload = function(url) {
-    if (this.debug) console.log('Preloader.abortPreload. url:', url, 'processing_queue:', this.processing_queue);
-    this.pauseQueue();
-    if (url === undefined) {
-      url = this.currently_loading_url;
-    }
-    var $element = this.$fromUrl(url);
-    // url = $source.attr('src');
-    $element.children('source').attr('src', '');
-    if ($element[0] && $element[0].load) {
-      $element[0].load();
-    }
-    $element.remove();
     
-    delete this.cache[url];
+    //this.cache = {}; // keyed by url. The value is null while being loaded, and the element when completed.
+    // this.callbacks = {}; // keyed by url, lists of callbacks for when a particular movie is done.
+    this.progresses = {}; // keyed by url, jQuery elements | undefined
+    
+    this.queue = urls.slice(0);
+    this.currently_loading_url = undefined;
+    this.paused = true;
+    // this.processing_queue = false;
+    this.timeouts = {zero: 20, slow: 80, hard: 200, wait: 200, loop: 250};
+  }
+  Preloader.prototype.pauseQueue = function(abort) {
+    this.paused = true;
+    var url = this.currently_loading_url;
+    if (this.debug) console.log('Preloader.pauseQueue. abort:', abort, ' url:', url);
+    if (url !== undefined && abort) {
+      var $element = this.$fromUrl(url);
+      $element.children('source').attr('src', '');
+      var media = $element[0];
+      if (media && media.load) media.load();
+      $element.remove();
+      this.queue.unshift(url);
+      this.currently_loading_url = undefined;
+    }
   };
-  Preloader.prototype.unpauseQueue = function() {
-    if (this.debug) console.log('Preloader.unpauseQueue.');
+  Preloader.prototype.resumeQueue = function() {
+    if (this.debug) console.log('Preloader.resumeQueue: pause was:', this.paused);
     this.paused = false;
-    this.processQueue();
+    this.queueLoop();
   };
-  Preloader.prototype.onLoaded = function(url, callback) {
-    // callback signature: function(err, raw_media_element) -- same as getMedia
-    // these will be called before getMedia's own callback is called
-    if (this.callbacks[url] === undefined) {
-      this.callbacks[url] = [];
-    }
-    this.callbacks[url].push(callback);
-  };
-  Preloader.prototype.processQueue = function(state) {
-    if (this.debug) {
-      console.log('Preloader.processQueue: processing_queue:', this.processing_queue,
-        'paused:', this.paused, 'state:', state);
-    }
-    // this should respect this.paused == false (it should not override it)
+  Preloader.prototype.queueLoop = function() {
     var preloader = this;
-    if (preloader.paused) {
-      preloader.processing_queue = false;
-    }
-    else if (preloader.processing_queue && state !== 'continue') {
-      preloader.processing_queue = false;
-    }
-    else {
-      preloader.processing_queue = true;
-      var next_url = null;
-      for (var ii = 0; ii < preload_limit && ii < preloader.urls.length; ii++) {
-        var url = preloader.urls[ii];
-        if (preloader.cache[url] === undefined) {
-          // it's neither queued nor already loaded.
-          next_url = url;
-          break;
-        }
-      }
-      if (next_url) {
-        if (preloader.debug) console.log('Preloader queue loading url:', next_url);
-        return preloader.getMedia({url: next_url}, function(err, media) {
+    var url = this.currently_loading_url;
+    // if (this.debug) console.log('Preloader.queueLoop. url:', url);
+    if (url === undefined) {
+      url = this.queue.shift(); // get the next in the queue
+      if (url !== undefined) {
+        if (this.debug) console.log('  ', this.queue.length, ' left. queueLoop -> getMedia:', url);
+        this.getMedia(url, false, function(err, media) {
           // just ignore the media for now, since it's in the cache
           if (err) { console.log(err); }
-          return preloader.processQueue('continue');
+          if (!preloader.paused)
+            preloader.queueLoop();
         });
       }
       else {
-        if (preloader.debug) {
-          console.log('Preloader.processQueue called, but the queue has already finished preloaded.');
-        }
-        preloader.processing_queue = false;
+        if (this.debug) console.log('  queueLoop: all done');
       }
+    }
+    else {
+      if (this.debug) console.log('  queueLoop: already loading ', url);
     }
   };
   Preloader.prototype.$fromUrl = function(url, make_if_missing) {
     var $element = $('#' + url.replace(/\W/g, ''));
     if (this.debug) console.log('Preloader.$fromUrl', url, $element.length, make_if_missing);
     if (!$element.length && make_if_missing === true) {
-      if (url.match(/\.(mp4|m4v)/)) {
+      var id = url.replace(/\W/g, '');
+      if (url.match(/\.(mp4|m4v)$/)) {
         $element = $('<video width="640" height="360" ' +
-          'id="' + url.replace(/\W/g, '') + '" ' +
+          'id="' + id + '" ' +
           'style="display: none" autobuffer preload="auto">' +
           '<source src="' + url + '" type="video/mp4"></video>').appendTo(this.$buffer);
       }
-      else {
-        $element = $('<audio id="' + url.replace(/\W/g, '') + '" ' +
+      else if (url.match(/\.(mp3|wav)$/)) {
+        $element = $('<audio id="' + id + '" ' +
           'style="display: none" autobuffer preload="auto">' +
           '<source src="' + url + '"></audio>').appendTo(this.$buffer);
+      }
+      else {
+        $element = $('<img id="' + id + '" style="display: none" ' +
+          'src="' + url + '" />').appendTo(this.$buffer);
       }
     }
     return $element;
   };
-  Preloader.prototype.updateProgress = function(url, done) {
-    var $progress = this.progresses[url];
-    if ($progress && $progress.length) {
-      $progress.html('<div class="progress-bar">' +
-        '<div style="width: ' + (done * 100).toFixed(0) + '%;">&nbsp;</div></div>');
-    }
+  Preloader.prototype._gotMedia = function(media, callback) {
+    if (this.debug) console.log('  _gotMedia');
+    preloader.currently_loading_url = undefined;
+    callback(undefined, media);
   };
-  Preloader.prototype.getMedia = function(options, callback) {
+  Preloader.prototype.getMedia = function(url, rush, callback) {
     // async, callback signature: function(err, element)
-    // options = { url: 'whatever', rush: undefined or true }
-    var url = options.url, media, preloader = this;
-
-    if (this.cache[url] !== undefined) {
-      if (this.cache[url] !== null) {
-        return callback(undefined, this.cache[url]);
-      }
-      else {
-        return this.onLoaded(url, callback);
-      }
-    }
-    this.cache[url] = null; // from undefined -> null
+    if (this.debug) console.log("Preloader.getMedia. url:", url, 'rush:', rush);
+    var preloader = this, media = this.$fromUrl(url, true)[0];
     this.currently_loading_url = url;
 
-    media = this.$fromUrl(url, true)[0];
-
-    if (preloader.debug) console.log("Preloader.getMedia id =", media.id);
-    
     var $progress, last_buffer_length = 0, buffer_diffs = [];
     (function bufferWatcher() {
+      if (preloader.queue.indexOf(url) > -1) {
+        if (preloader.debug) console.log("  bufferWatcher: media aborted:", media);
+        return callback(undefined, 'media aborted, retry');
+      }
       if (!media) {
+        if (preloader.debug) console.log("  bufferWatcher: missing media:", media);
         return callback({type: 'UrlError',
           message: 'Media cannot be inserted with default html for that url: ' + url});
       }
@@ -219,30 +183,23 @@ function timestamp() { return (new Date()).getTime(); }
         last_10_diff += buffer_diffs[j];
       }
       
-      preloader.updateProgress(url, isNaN(done) ? 0 : done);
-
-      var finish = function() {
-        if (preloader.debug) console.log("Preload.getMedia.finish:", url);
-        preloader.cache[url] = media; // from null -> cached
-        preloader.currently_loading_url = '';
-        if (preloader.callbacks[url] !== undefined) {
-          $.each(preloader.callbacks[url], function(i, loaded_callback) {
-            loaded_callback(undefined, media);
-          });
-        }
-        callback(undefined, media);
-      };
-
+      var $progress = preloader.progresses[url];
+      if ($progress && $progress.length) {
+        $progress.html('<div class="progress-bar">' +
+          '<div style="width: ' + ((isNaN(done) ? 0 : done) * 100).toFixed(0) + '%;">&nbsp;</div></div>');
+      }
+      
       if (done > 0.99) {
-        finish(); // normal completion
+        return preloader._gotMedia(media, callback); // normal completion
       }
       // test if anything crazy has happened
-      else if (preloader.cache[url] === undefined) {
-        callback("That media was deleted from the DOM. Exiting the buffer process.");
-      }
-      else if (options.rush === true) {
+      // else if (preloader.cache[url] === undefined) {
+        // callback("That media was deleted from the DOM. Exiting the buffer process.");
+      // }
+      else if (rush === true) {
         if (calls > 10 && last_10_diff < 0.01 && done > 0.5) {
-          finish(); // sometimes Chrome doesn't feel like loading the whole movie. Okay, fine.
+          // sometimes Chrome doesn't feel like loading the whole movie. Okay, fine.
+          return preloader._gotMedia(media, callback);
         }
         else {
           setTimeout(bufferWatcher, preloader.timeouts.loop);
@@ -250,23 +207,24 @@ function timestamp() { return (new Date()).getTime(); }
       }
       else {
         if (calls > preloader.timeouts.zero && last_10_diff < 0.01 && done > 0.5) {
-          finish(); // it seems that Chrome isn't ever going to preload the whole media
+          // it seems that Chrome isn't ever going to preload the whole media
+          return preloader._gotMedia(media, callback);
         }
         else if (calls > preloader.timeouts.slow && buffer_length === 0) {
           console.log("The media cannot be found or loaded.", url, media);
           callback(undefined, '<p>This media is missing.</p>');
         }
         else if (calls > preloader.timeouts.slow && last_10_diff < 0.01) {
-          finish();
+          return preloader._gotMedia(media, callback);
         }
         else if (calls > preloader.timeouts.hard) {
-          finish();
+          return preloader._gotMedia(media, callback);
         }
         else if (calls > preloader.timeouts.wait) {
           console.log("The media is taking longer than " +
             preloader.timeouts.wait * (preloader.timeouts.loop / 1000) +
             " seconds to load. It'll just have to play, nonetheless.");
-          finish();
+          return preloader._gotMedia(media, callback);
         }
         else {
           setTimeout(bufferWatcher, preloader.timeouts.loop);
@@ -356,7 +314,6 @@ function timestamp() { return (new Date()).getTime(); }
     var triangle_radius = parseInt($triangle.css('border-top-width'), 10);
 
     // console.log('bubble_id', id, 'triangle_radius', triangle_radius, 'bubble_size', bubble_size);
-    // return;
     
     if (options.anchor === 'r') {
       $triangle.css({
