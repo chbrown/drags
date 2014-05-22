69421e6870f6d9a8143c19c55efbd4591a89a8a7
Christopher Brown <io@henrian.com>
2014-05-22 10:54:22 -0500

    Modernize jslint comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
-'use strict'; /*jslint es5: true, node: true, indent: 2 */
-
+/*jslint node: true */
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
--- bin/drags-commands.js
+++ bin/drags-commands.js
@@ -1,4 +1,4 @@
-'use strict'; /*jslint es5: true, node: true, indent: 2 */
+/*jslint node: true */
 var fs = require('fs');
 var async = require('async');
 var cluster = require('cluster');
